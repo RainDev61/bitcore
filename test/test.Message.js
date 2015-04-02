2ea86e25f8495319a0856a37259773233cdda6c8
Luke <lukem95@gmail.com>
2015-04-02 09:46:40 +0100

    Converted message bytes to StartCOIN

--- test/test.Message.js
+++ test/test.Message.js
@@ -66,8 +66,8 @@ describe('Message', function() {
   });
 
   describe('magicBytes', function() {
-    it('should be "Bitcoin Signed Message:\\n"', function() {
-      Message.magicBytes.toString().should.equal('Bitcoin Signed Message:\n');
+    it('should be "StartCOIN Signed Message:\\n"', function() {
+      Message.magicBytes.toString().should.equal('StartCOIN Signed Message:\n');
     });
   });
 
